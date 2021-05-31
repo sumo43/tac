7c3bd3e3c4d3bfe4f9ec908fb4af8f9a94ea8b7a
yatsenkoa <yatsenkoa2020@gmail.com>
2021-05-31 16:49:07 -0400

    minimax works

--- src/Game.js
+++ src/Game.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 import './Game.css'
 
 const pprint = (num) => {
-    if (num == 2) return 'E';
+    if (num == 2) return ' ';
     else if(num == 0) return 'O';
     else if(num == 1) return 'X';
 }
 
+//this is fine
 const isFull = (state) => {
     let check = true
     let i;
@@ -20,84 +21,143 @@ const isFull = (state) => {
     return check 
 }
 
+//this is fine
 const won = (state) => { 
-    let i;
-    let sums = []
-    let sum;
-
-    for(i = 0; i < 3; i++) {
-        sum = state[i][0] + state[i][1] + state[i][2]        
-        sums.push(sum)
-    }
-
-    for(i = 0; i < 3; i++) {
-        sum = state[0][i] + state[1][i] + state[2][i]
-        sums.push(sum)
-    }
-
-    sums.push(state[0][0] + state[1][1] + state[2][2])
-    sums.push(state[0][2] + state[1][1] + state[2][0])
-
+    let i = 0;
     let ret = -1
 
-    sums.forEach((sum) => 
-    {
-        if(sum == 3) {
+    for(i = 0; i < 3; i++) {
+        if(state[i][0] == 1 && state[i][1] == 1 && state[i][2] == 1) {
             ret = 1
         }
-        else if(sum == 0) {
+        else if(state[i][0] == 0 && state[i][1] == 0 && state[i][2] == 0) {
             ret = 0
         }
-    })
+    }
+
+    for(i = 0; i < 3; i++) {
+        if(state[0][i] == 1 && state[1][i] == 1 && state[2][i] == 1) {
+            ret = 1
+        }
+        else if(state[0][i] == 0 && state[1][i] == 0 && state[2][i] == 0) {
+            ret = 0
+        }
+    }
+
+    if(state[0][0] == 0 && state[1][1] == 0 && state[2][2] == 0) ret = 0
+    else if(state[0][0] == 1 && state[1][1] == 1 && state[2][2] == 1) ret = 1
+
+
+    if(state[0][2] == 0 && state[1][1] == 0 && state[2][0] == 0) ret = 0
+    else if(state[0][2] == 1 && state[1][1] == 1 && state[2][0] == 1) ret = 1
+    
+    if(isFull(state)) {
+        ret = 2
+    }
+    
     return ret
 }
 
-const AIMin = (state) => {
-    
-    if(isFull(state))
-    {
-        let w = won(state)
-        if(w == 0) return -1
-        if(w == 1) return 1
-        if(w == -1) {
-            return 0
+const arrayMax = (array) => {
+    let max = -4000
+    let maxIndex = 2
+    let i
+
+    for(i = 0; i < array.length; i++) {
+        if(array[i] >= max) {
+            max = array[i]
+            maxIndex = i
         }
     }
 
-    let neighbors = n(state, 1)
-    let neighborAIMax = neighbors.map(x => AIMax(x))
-    let i = neighborAIMax.indexOf(Math.min(neighborAIMax))
 
-    return neighbors[i]
-
+    return maxIndex
 }
 
-const AIMax = (state) => {
-     
-    if(isFull(state))
-    {
-        let w = won(state)   
-        if(w == 0) return 1
-        if(w == 1) return -1
-        if(w == -1) {
-            return 0
+const arrayMin = (array) => {
+    let min = 40000
+    let minIndex = 2
+    let i
+
+    for(i=0; i < array.length; i++) {
+        if(array[i] <= min) {
+            min = array[i]
+            minIndex = i
         }
-        
     }
 
-    let neighbors = n(state, 0)
-    let neighborAIMin = neighbors.map(x => AIMin(x))
-    let i = neighborAIMin.indexOf(Math.max(neighborAIMin))
+    return minIndex
+}
 
+const arrMap = (array, func) => {
+    let arr = []
+    let i
+    for(i=0; i < array.length; i++) {
+        arr.push(func(array[i]))
+    }
 
+    return arr
+}
 
-    return neighbors[i]
+const minvalue = (state) => {
+    
+    let w = won(state)
+    if(w == 0) {
+        console.log(state)
+        return 1
+    } 
+    else if(w == 1) return -1
+    else if(w == 2) {
+        return 0
+    }
+    else {
+        let neighbors = n(state, 1)
+        let neighborMaxvalue = arrMap(neighbors, maxvalue)
+
+        let min = arrayMin(neighborMaxvalue)
+        return min
+    }
+    
+}
+
+//separate out minimax and algorithm functions - minimax only returns numbers per board, etc. 
+
+const maxvalue = (state) => { 
+
+    let w = won(state)   
+    if(w == 0) return 1 
+    else if(w == 1) return -1
+    else if(isFull(state)) {
+        return 0
+    }
+    else
+    {
+        let neighbors = n(state, 0)
+        let neighborMinvalue = arrMap(neighbors, minvalue)
+
+        let max = arrayMax(neighborMinvalue)
+        return max
+    }
+    
+}
+
+//starting with computer
+const minimax = (state, i) => {
+
+    if (i == 0) {
+
+        //max of min values of all of the neighbors
+        let neighbors = n(state, 0)
+        let maxIndex = arrayMax(arrMap(neighbors, minvalue))
+        return neighbors[maxIndex]
+    }
+
 }
 
 const n = (state, p) => {
     let nei = []
-    let i = 0
-    let j = 0
+    let i 
+    let j 
     for(i = 0; i < 3; i++) {
         for(j = 0; j < 3; j++) {
             if(state[i][j] == 2) {
@@ -107,18 +167,16 @@ const n = (state, p) => {
             }
         }
     }
-
+    
     return nei
 }
-
-
-
 
 var state = [[2,2,2], [2,2,2], [2,2,2]]
 class Game extends React.Component {
     constructor() {
         super()
         this.forceUpdateHandler = this.forceUpdateHandler.bind(this)
+        this.restartHandler = this.restartHandler.bind(this)
         this.game()
     }
 
@@ -126,19 +184,23 @@ class Game extends React.Component {
         this.render() 
     }
 
+    restartHandler() {
+        state = [[2,2,2], [2,2,2], [2,2,2]] 
+        this.forceUpdate()
+    }
+
     forceUpdateHandler() {
         this.forceUpdate()
     }
 
-
     action (one, two) {
         //human turn
+        //computer turn
         state[one][two] = 1
 
-        
-        //computer turn
-        let compState = AIMax(state)
-        state = compState
+
+        var optimalAction = minimax(state, 0)
+        state = optimalAction
 
         this.forceUpdate();
     }
@@ -184,7 +246,7 @@ class Game extends React.Component {
                         <button class='button' style={{fontSize:`10px`}} onClick={() => {this.action(2, 2)}}></button>
                     </div>
                 </div>
-                <div class='footer'>
+                <div class='footer' onClick ={this.restartHandler}>
                     <button>RESTART</button>
                 </div>
             </div>
